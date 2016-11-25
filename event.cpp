e0f330b0c73950ac12d71fce1c02cbd65e71a72d
Stinesc <limarevigor@gmail.com>
2016-11-25 13:12:15 +0200

    вот

--- event.cpp
+++ event.cpp
@@ -1,6 +1,22 @@
 #include "event.h"
-
-Event::Event()
+Event::Event(QString name_of_event, date ds, date de)
 {
-
+    name = name_of_event;
+    date_of_start.day = ds.day;
+    date_of_start.month = ds.month;
+    date_of_start.year = ds.year;
+    date_of_end.day = de.day;
+    date_of_end.month = de.month;
+    date_of_end.year = de.year;
+}
+Event::Event(QString name_of_event, date ds, date de, QString com)
+{
+    name = name_of_event;
+    date_of_start.day = ds.day;
+    date_of_start.month = ds.month;
+    date_of_start.year = ds.year;
+    date_of_end.day = de.day;
+    date_of_end.month = de.month;
+    date_of_end.year = de.year;
+    comment = com;
 }
